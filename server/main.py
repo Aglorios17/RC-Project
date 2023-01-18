a4bb071a9fae09d89d48806f71e76bd0c7356171
Aglorios17 <alessio.glorioso17@gmail.com>
2023-01-18 19:26:20 +0100

    user0.3

--- server/main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-import time
-import threading
-
-if __name__ == "__main__":
-#    t = BackgroundTasks()
-#    t.start()
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
